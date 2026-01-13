a4afb3886e195dacf13ccabe1d1325945ef35006
blackheartHN <nguyentran55it@gmail.com>
2026-01-13 20:07:23 +0700

    Added win/lose sound

--- Assets/Scripts/Utilities/ZombieWinUI.cs
+++ Assets/Scripts/Utilities/ZombieWinUI.cs
@@ -50,11 +50,13 @@ public class ZombieWinUI : MonoBehaviour
             // Use assigned UI elements
             gameOverText.text = "2AN DIES MID!";
             gameOverPanel.SetActive(true);
+            NetworkGameManager.Instance.PlaySoundClientRpc("zombie_win");
         }
         else
         {
             // Fallback: Create UI dynamically
             CreateDynamicZombieWinUI("2AN DIES MID!");
+            NetworkGameManager.Instance.PlaySoundClientRpc("zombie_win");
         }
     }
 
